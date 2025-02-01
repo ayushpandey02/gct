a69e3df80d909246045feab571f5d9a46e6e1bbf
ayushpandey02 <ayushpaandey.01@gmail.com>
2025-02-01 13:02:19 +0530

    final commit

--- script.js
+++ script.js
@@ -293,7 +293,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         formData.append('photo', photoFile);
                         formData.append('paymentScreenshot', screenshotFile);
             
-                        const response = await fetch('https://gct-backend-phi.vercel.app/api/v1/user/submmit-form', {
+                        const response = await fetch('https://gct-backend-phi.vercel.app/api/v1/user/submit-form', {
                             method: 'POST',
                             credentials: 'omit',  // Changed from 'include' to 'omit'
                             body: formData
